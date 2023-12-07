db7353b2f853628c4effe35c8b0a34b02cc65cb3
labuser@candela <labuser@candela.hhg.lab>
2023-12-07 16:37:57 +0100

    initial commit

--- register_device.py
+++
@@ -1,9 +0,0 @@
-import tango
-
-dev_info = tango.DbDevInfo()
-dev_info.server = "Cryostat/test"
-dev_info._class = "Cryostat"
-dev_info.name = "test/cryo/1"
-
-db = tango.Database()
-db.add_device(dev_info)
